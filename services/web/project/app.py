902158090c2c0667fe59ca72626d92db67a69dbb
Laxminarayan R <ln.raval01@gmail.com>
2022-08-09 12:05:23 +0530

    App Creation

---
+++ services/web/project/app.py
@@ -0,0 +1,21 @@
+from flask import jsonify
+
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
+# from flask_migrate import Migrate
+
+from project import create_app, db
+from project.controllers.user import user_bp
+
+app = create_app()
+
+# db = SQLAlchemy(app)
+# migrate = Migrate(app)
+# CORS(app)
+# print("db :", db)
+
+@app.route("/")
+def index():
+    return jsonify(message="Welcome to docker-flask"), 200
+
+app.register_blueprint(user_bp, url_prefix="/user")
